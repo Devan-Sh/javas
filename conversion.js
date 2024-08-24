55cb57fee6699f7d6ae287a8624212f02e618e5f
Devan-Sh <149086182+Devan-Sh@users.noreply.github.com>
2024-08-24 21:52:56 +0000

    conversion

---
+++ conversion.js
@@ -0,0 +1,15 @@
+let somn = 33
+let som = String(somn)
+console.log(typeof som)
+console.log(som);
+/* so if i do it on boolean 
+1 true 0 false
+if on string
+on "" false
+on"soomeme" true
+on undefined it gives value nan
+on null it gives number 0
+basically*/
+let a = null
+let s = Number(a);
+console.log(s);
